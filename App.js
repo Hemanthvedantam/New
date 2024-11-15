8589e49b43683479d88c2498a39d626c58e7bad6
Hemanthvedantam <154527901+Hemanthvedantam@users.noreply.github.com>
2024-11-15 12:04:46 +0530

    app.js uploaded

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard/*" element={<Dashboard />} /> {/* Enable nested routes */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
